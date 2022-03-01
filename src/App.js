09de044fc2fd7deb1241c67a185ffeda052b2a8c
nakimera <prossienakimera@gmail.com>
2022-03-01 12:27:36 +0300

    update button component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import {Button} from './components/button';
+import {Button, RoundedButtons} from './components/button';
 
 function App() {
   return (
     <>
       <Button>hello world</Button>
+      <Button primary>hello world</Button>
+      <Button danger>hello world</Button>
+      <RoundedButtons>rounded</RoundedButtons>
+      <Button rounded>another rounded button</Button>
     </>
   );
 }
